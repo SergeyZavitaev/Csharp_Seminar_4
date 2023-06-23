94729a695d905d387104b7aa9225df8909296759
Sergey Zavitaev <nikgala@list.ru>
2023-06-23 19:57:31 +0300

    task 2 is completed

---
+++ Task_2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число: ");
+string number_str = Console.ReadLine();
+char[] number_array = number_str.ToCharArray();
+int result_sum = 0;
+foreach (char i in number_array)
+    result_sum += Convert.ToInt32(char.ToString(i));
+Console.Write(result_sum);
