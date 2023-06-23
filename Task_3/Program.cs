c13dc02dfcf613740a0cef3fb1edeccc15a7c92c
Sergey Zavitaev <nikgala@list.ru>
2023-06-23 20:04:04 +0300

    task 3 is completed

---
+++ Task_3/Program.cs
@@ -0,0 +1,8 @@
+﻿Random rand = new Random();
+int count = rand.Next(0, 25);
+int[] massive = new int[count];
+for (int i = 0; i < massive.Length; i++)
+{
+    massive[i] = rand.Next(0, 100);
+    Console.Write($"{massive[i]}, ");
+}
