4299d4b61696f5bb03a3dcfb2af726217515939d
Sergey Zavitaev <nikgala@list.ru>
2023-06-23 19:54:06 +0300

    task 1 is completed

---
+++ Task_1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 0; i < degree; i++)
+    result = result * number;
+Console.Write(result);
